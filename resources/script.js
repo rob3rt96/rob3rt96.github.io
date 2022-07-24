c9ba37775b25874d7659f46998cc1c4e9daea0d6
rob3rt96 <robert.popescu195@gmail.com>
2022-07-24 10:06:21 +0300

    Add modification nr. 7 - site version 2

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,25 @@
 const date = new Date();
 const elementCurrentDate = document.getElementById('current-date');
 
+const navToggle = document.getElementsByClassName('nav-toggle')[0];
+const navLinks = document.querySelectorAll('.nav__link');
+
+
+
 elementCurrentDate.textContent = date.getFullYear();
+
+const openNav = () => {
+    document.body.classList.toggle('nav-open');
+};
+
+const removeNav = () => {
+    document.body.classList.remove('nav-open');
+};
+
+
+// Nav toggle event listener
+navToggle.addEventListener('click', openNav);
+
+navLinks.forEach(link => {
+    link.addEventListener('click', removeNav);
+});
