d3310a6025d7c9c6aaab964c010e53f82afcc286
rob3rt96 <robert.popescu195@gmail.com>
2022-07-25 17:25:09 +0300

    Add modification nr. 10 - site version 2

--- resources/script.js
+++ resources/script.js
@@ -4,7 +4,8 @@ const elementCurrentDate = document.getElementById('current-date');
 const navToggle = document.getElementsByClassName('nav-toggle')[0];
 const navLinks = document.querySelectorAll('.nav__link');
 
-
+const contactLink = document.querySelectorAll('.contact__link');
+const closeContact = document.getElementsByClassName('contact-close')[0];
 
 elementCurrentDate.textContent = date.getFullYear();
 
@@ -16,6 +17,14 @@ const removeNav = () => {
     document.body.classList.remove('nav-open');
 };
 
+const openContact = () => {
+    document.body.classList.toggle('contact-open');
+};
+
+const removeContact = () => {
+    document.body.classList.remove('contact-open');
+};
+
 
 // Nav toggle event listener
 navToggle.addEventListener('click', openNav);
@@ -23,3 +32,9 @@ navToggle.addEventListener('click', openNav);
 navLinks.forEach(link => {
     link.addEventListener('click', removeNav);
 });
+
+contactLink.forEach(link => {
+    link.addEventListener('click', openContact);
+});
+
+closeContact.addEventListener('click', removeContact);
